24a61f27071a67d5423efc0bc202a639e6fecc2c
Ateeb <ateeb@live.no>
2025-01-26 22:54:08 +0100

    done

--- workshop.wwwapi/Models/Doctor.cs
+++ workshop.wwwapi/Models/Doctor.cs
@@ -4,10 +4,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace workshop.wwwapi.Models
 {
     //TODO: decorate class/columns accordingly    
+
+    [Table("doctors")]
     public class Doctor
     {
         [Key]
-        public int Id { get; set; }        
+        public int Id { get; set; }
+
+        [Column("name")]
         public string FullName { get; set; }
+
+        //[ForeignKey(nameof(Appointments))]
+        public List<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+
     }
 }
